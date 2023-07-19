f78d65b13e32959cf3d065e5f6a93681da313cf1
bradonstudie <bradon.studebaker@gmail.com>
2023-07-19 08:49:10 -0400

    update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import logo from "../../public/logo.png";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Elaine Elaine</h1>
+      <div className={styles.center}>
+        <Image src={logo} alt="Elaine Elaine" className={`${styles.logo}`} />
+      </div>
     </main>
   );
 }
