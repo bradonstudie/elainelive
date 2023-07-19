f300f7d9efff1a00d3ee201d22d6c37397067bbc
bradonstudie <bradon.studebaker@gmail.com>
2023-07-19 19:09:19 -0400

    Add layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,61 @@
-import Image from "next/image";
-import logo from "../../public/logo.png";
+import Link from "next/link";
 import styles from "./page.module.css";
+
+interface MenuItem {
+  title: string;
+  link: string;
+}
+
+const menuItems: MenuItem[] = [
+  {
+    title: "Music",
+    link: "/music",
+  },
+  {
+    title: "Store",
+    link: "/store",
+  },
+  {
+    title: "Contact",
+    link: "/contact",
+  },
+  {
+    title: "About",
+    link: "/about",
+  },
+];
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.center}>
-        <Image src={logo} alt="Elaine Elaine" className={`${styles.logo}`} />
+        <h1 style={{ marginBottom: "1rem" }}>elaine elaine</h1>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+            maxWidth: "35rem",
+          }}
+        >
+          {menuItems.map((item) => (
+            <Link key={item.title} href={item.link}>
+              <h2 style={{ fontWeight: "lighter" }}>{item.title}</h2>
+            </Link>
+          ))}
+        </div>
+      </div>
+
+      <div style={{ textAlign: "center" }}>
+        <p style={{ marginBottom: "0.25rem;" }}>
+          If you study the material on this website, you will understand the
+          secrets of the universe.
+        </p>
+        <b>
+          <p>&copy;2023 Elaine Elaine</p>
+        </b>
       </div>
     </main>
   );
