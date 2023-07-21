34432972db94772947107b1d6902e03b8d491b30
bradonstudie <bradon.studebaker@gmail.com>
2023-07-21 09:58:44 -0400

    add about description to component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,4 @@
+import { AboutDescription } from "@/components/AboutDescription";
 import { ImageDisplay } from "@/components/ImageDisplay";
 import aboutGif from "../../../public/about.gif";
 import aboutOne from "../../../public/about-1.jpg";
@@ -21,25 +22,7 @@ export default function About() {
                     padding: "1rem",
                 }}
             >
-                <p>
-                    <b style={{ fontSize: "2rem", fontFamily: "sans-serif" }}>
-                        <span style={{ fontSize: "3rem" }}>O</span>n the evening
-                        of the last day of October, 1501
-                    </b>
-                    <br />
-                    Cesare Borgia arranged a banquet in his chambers in the
-                    Vatican with &quot;fifty honest prostitutes&quot;,[2] called
-                    courtesans, who danced after dinner with the attendants and
-                    others who were present, at first in their garments, then
-                    naked.[3] After dinner the candelabra with the burning
-                    candles were taken from the tables and placed on the floor,
-                    and chestnuts were strewn around, which the naked courtesans
-                    picked up, creeping on hands and knees between the
-                    chandeliers, while the Pope, Cesare, and his sister Lucretia
-                    looked on. Finally, prizes were announced for those who
-                    could perform the act most often with the courtesans, such
-                    as tunics of silk, shoes, barrets, and other things.
-                </p>
+                <AboutDescription />
             </div>
         </>
     );
---
+++ src/components/AboutDescription.tsx
@@ -0,0 +1,19 @@
+export const AboutDescription = () => (
+    <p>
+        <b style={{ fontSize: "2rem", fontFamily: "sans-serif" }}>
+            <span style={{ fontSize: "3rem" }}>O</span>n the evening of the last
+            day of October, 1501
+        </b>
+        <br />
+        Cesare Borgia arranged a banquet in his chambers in the Vatican with
+        &quot;fifty honest prostitutes&quot;,[2] called courtesans, who danced
+        after dinner with the attendants and others who were present, at first
+        in their garments, then naked.[3] After dinner the candelabra with the
+        burning candles were taken from the tables and placed on the floor, and
+        chestnuts were strewn around, which the naked courtesans picked up,
+        creeping on hands and knees between the chandeliers, while the Pope,
+        Cesare, and his sister Lucretia looked on. Finally, prizes were
+        announced for those who could perform the act most often with the
+        courtesans, such as tunics of silk, shoes, barrets, and other things.
+    </p>
+);
